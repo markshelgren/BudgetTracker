5b527ab8dff7c7c4b2974fd8714c7f9e9cf4e06f
Mark Shelgren <markshelgren@outlook.com>
2021-01-31 11:38:30 -0800

    Fix testing issues and bugs

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-//mongodb://<dbuser>:<dbpassword>@ds133627.mlab.com:33627/heroku_fzxcrdvs
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+	useNewUrlParser: true,
+	useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
